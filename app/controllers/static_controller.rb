b2053d8b614cef847bff96e534e155fc5f3d8923
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-06-20 12:19:37 -0600

    add production deployment

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,10 @@
-class StaticController < ApplicationController
+require "rails/application_controller"
+
+class StaticController < Rails::ApplicationController
+  layout false
+
+  # dishing up React code
   def index
+    render file: Rails.root.join('public', 'index.html')
   end
 end
